19ce14e0a43270f0751d52c82bb8b125c248c430
mrxhook <derrysucari01@gmail.com>
2022-10-14 18:11:11 -0500

    fade in effect component setup

---
+++ src/components/fade-in-effect/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  overflow: hidden scroll;
+  width: 100%;
+  max-width: 1920px;
+  height: ${({ theme }) => `${theme.dimensions.innerHeight}px`};
+  box-shadow: 0px 4px 20px var(--second-dark);
+  margin: 0 auto;
+  scrollbar-width: none;
+  ::-webkit-scrollbar{
+    display: none;
+  }
+  :focus-visible{
+    outline: none;
+  }
+`;
