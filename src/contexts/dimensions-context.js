188d7fe2727a018e27382a628df71532a629fa50
mrxhook <derrysucari01@gmail.com>
2022-10-14 18:16:53 -0500

    dimensions context setup

---
+++ src/contexts/dimensions-context.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+/* Theme */
+import { ThemeProvider } from 'styled-components';
+
+const useDimensions = () => {
+  const [ dimensions, setDimensions ] = React.useState({ innerWidth, innerHeight });
+  
+  const resize = React.useCallback(() => setDimensions({ innerWidth, innerHeight }), []);
+
+  React.useLayoutEffect(() => {
+    resize();
+    addEventListener('resize', resize);
+    return () => {
+      removeEventListener('resize', resize);
+    }
+  }, []);
+
+  return dimensions;
+}
+
+export const DimensionsProvider = ({ children }) => {
+  if(typeof window === 'undefined') return null;
+  const dimensions = useDimensions();
+  return (
+    <ThemeProvider theme={{ dimensions }}>
+      { children }
+    </ThemeProvider>
+  );
+}
