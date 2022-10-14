9ad267ccd01e62379a16b427a44abfc9f1751bb7
mrxhook <derrysucari01@gmail.com>
2022-10-14 18:18:01 -0500

    main layout footer setup

---
+++ src/layouts/main-layout/footer/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+/* Gatsby */
+import { Link as GatsbyLink } from 'gatsby';
+
+export const Footer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 30px 20px;
+  @media screen and (max-width: 768px){
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const TextLine = styled.p`
+  text-align: center;
+  font-size: 20px;
+  @media screen and (max-width: 1366px){
+    font-size: 18px;
+  }
+  @media screen and (max-width: 768px){
+    margin: 0 0 30px;
+  }
+  @media screen and (max-width: 460px){
+    font-size: 15px;
+  }
+`;
+
+export const FooterLink = styled(GatsbyLink)`
+  text-decoration: underline;
+`;
