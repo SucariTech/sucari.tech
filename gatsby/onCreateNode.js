f57ad9a5ef5cb832a8e8927cb6017f12998307a0
mrxhook <derrysucari01@gmail.com>
2022-10-14 18:05:57 -0500

    gatsby.onCreateNode setup

---
+++ gatsby/onCreateNode.js
@@ -0,0 +1,17 @@
+const { createFilePath } = require('gatsby-source-filesystem');
+
+module.exports = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+
+  if([ 'Mdx', 'Json' ].includes(node.internal.type)){
+    const value = createFilePath({
+      node,
+      getNode
+    });
+    createNodeField({
+      node,
+      name: 'slug',
+      value
+    });
+  }
+}
