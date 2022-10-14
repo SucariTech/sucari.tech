eebb137526a7654a8403c995efcd5694760f2d00
mrxhook <derrysucari01@gmail.com>
2022-10-14 17:55:12 -0500

    gatsby-config.js setup

---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+const { resolve } = require('path');
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://sucari.tech'
+  },
+  plugins: [
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-json',
+      options: {
+        typeName: 'Json'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: resolve(__dirname, './content')
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-styled-components',
+      options: {
+        displayName: false
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /\.svg$/,
+        }
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Sucari Tech',
+        short_name: 'sucari.tech',
+        crossOrigin: 'use-credentials',
+        icon: resolve(__dirname, './static/images/logo.png')
+      }
+    },
+    process.env.GOOGLE_ANALYTICS_ID && {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
+    }
+  ].filter(Boolean)
+};
