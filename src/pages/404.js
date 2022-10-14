9749530b10818f6ed58782c17c942088e79f7eb6
mrxhook <derrysucari01@gmail.com>
2022-10-14 18:09:36 -0500

    404 page setup

---
+++ src/pages/404.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+/* Gatsby */
+import { Link } from 'gatsby';
+
+/* Styled Components */
+import styled, { css } from 'styled-components';
+
+/* Helmet */
+import { Helmet } from 'react-helmet';
+
+/* Config */
+import config from '@config';
+
+/* Styles */
+const lineBase = css`
+  margin: 10px 0;
+  letter-spacing: 1px;
+`;
+
+const Container = styled.main`
+  padding: 20px;
+  margin: 5%;
+  p, a{
+    ${lineBase}
+    font-size: 18px;
+  }
+  a{
+    text-decoration: underline;
+    font-style: italic;
+    color: var(--third-light);
+  }
+`;
+
+const Wrapper = styled.div`
+  margin: 40px 0;
+`;
+
+const Title = styled.h1`
+  ${lineBase}
+  font-size: 30px;
+`;
+
+const Subtitle = styled.h2`
+  ${lineBase}
+  font-size: 22px;
+  font-weight: lighter;
+  font-style: italic;
+`;
+
+const PageNotFound = () => {
+  return (
+    <>
+      <Helmet htmlAttributes={{
+        lang: config.language,
+        translate: 'no'
+      }}>
+        <title>Sucari Tech - Page Not Found</title>
+
+        { /* Fonts */
+          config.fonts.map((src, index) => (
+            <link
+              key={ index }
+              href={ src }
+              rel="preload"
+              as="font"
+              crossOrigin=""
+            />
+          ))
+        }
+      </Helmet>
+      <Container>
+        <Title>Page Not Found</Title>
+        <Subtitle>Sorry, we couldn't find what you were looking for</Subtitle>
+        <Wrapper>
+          <p><strong>Need help finding something?</strong></p>
+          <p>Email info@sucari.tech and we’ll help you.</p>
+        </Wrapper>
+        <Link to="/">— Back to home</Link>
+      </Container>
+    </>
+  );
+}
+
+export default PageNotFound;
