67b84597f7bb5f54b2e74adfbad273fe8c0fcb27
mrxhook <derrysucari01@gmail.com>
2022-10-14 18:17:03 -0500

    contexts setup

---
+++ src/contexts/index.js
@@ -0,0 +1,20 @@
+/* Alert Context */
+export {
+  AlertProvider,
+  useAlert
+} from './alert-context';
+
+/* API Context */
+export {
+  APIProvider,
+  useAPI
+} from './api-context';
+
+/* Content Context */
+export {
+  ContentProvider,
+  useContent
+} from './content-context';
+
+/* Dimensions Context */
+export { DimensionsProvider } from './dimensions-context';
