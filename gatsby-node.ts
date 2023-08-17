f2933d08abb2c5fb8047ec00e44dc3a2303e9438
Derry Sucari <derrysucari01@gmail.com>
2023-08-17 09:16:02 -0500

    chore: update `gatsby-node.ts`

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,128 +1,60 @@
 import path from 'path'
 
 /* Dependencies */
-import type { GatsbyNode, Node } from 'gatsby'
-import { createFilePath } from 'gatsby-source-filesystem'
+import type { GatsbyNode } from 'gatsby'
 import TsconfigPathsWebpackPlugin from 'tsconfig-paths-webpack-plugin'
-
-interface CustomNodeFields {
-  slug: string
-}
-
-interface CustomNode extends Node {
-  fields: CustomNodeFields
-}
-
-interface CommonResult {
-  nodes: Array<CustomNode>
-}
-
-interface MDXResultData {
-  allMdx: CommonResult
-}
-
-interface JSONResultData {
-  allJson: CommonResult
-}
 
 export const createPages: GatsbyNode['createPages'] = async ({
   graphql,
   actions,
 }) => {
   const { createPage } = actions
+  const languageCode = process.env.GATSBY_LANGUAGE_CODE ?? 'en'
 
-  /* MDX */
-  const MDXResult = await graphql<MDXResultData>(`
-    query {
-      allMdx {
-        nodes {
-          id
-          fields {
-            slug
-          }
-          internal {
-            contentFilePath
-          }
-        }
-      }
-    }
-  `)
-
-  if (MDXResult.errors) {
-    console.error(MDXResult.errors)
-    throw Error(MDXResult.errors)
-  }
-
-  /* MDX Templates */
-  const privacyPolicyPageTemplate = path.resolve(
-    __dirname,
-    'src',
-    'templates',
-    'privacy-policy',
-    'index.tsx',
-  )
-
-  MDXResult.data?.allMdx.nodes.forEach(node => {
-    let template = null
-    const path = node.fields.slug
-
-    const slug = node.fields.slug
-
-    if (slug.includes('privacy-policy/')) {
-      template = privacyPolicyPageTemplate
-    }
-
-    if (template) {
-      createPage({
-        path,
-        component: `${template}?__contentFilePath=${node.internal.contentFilePath}`,
-        context: {
-          id: node.id,
-        },
-      })
-    }
-  })
-
-  /* JSON */
-  const JSONResult = await graphql<JSONResultData>(`
-    query {
-      allJson {
-        nodes {
-          id
-          fields {
+  /* Contentful Pages */
+  const contentfulPagesResult = await graphql<
+    Queries.ContentfulPagesQuery,
+    Queries.ContentfulPagesQueryVariables
+  >(
+    `
+      query ContentfulPages($languageCode: String) {
+        allContentfulPage(filter: { node_locale: { eq: $languageCode } }) {
+          nodes {
+            id
             slug
           }
         }
       }
-    }
-  `)
+    `,
+    {
+      languageCode,
+    },
+  )
 
-  if (JSONResult.errors) {
-    console.error(JSONResult.errors)
-    throw Error(JSONResult.errors)
+  if (contentfulPagesResult.errors) {
+    console.error(contentfulPagesResult.errors)
+    throw Error(contentfulPagesResult.errors)
   }
 
-  /* JSON Templates */
-  const homePageTemplate = path.resolve(
+  /* Landing Page Template */
+  const landingPageTemplate = path.resolve(
     __dirname,
     'src',
     'templates',
-    'home',
+    'landing-page',
     'index.tsx',
   )
 
-  JSONResult.data?.allJson.nodes.forEach(node => {
-    let template = null
-    let path = node.fields.slug
+  contentfulPagesResult.data?.allContentfulPage.nodes.forEach(node => {
+    let template: string | null = null
+    let path: string | null = null
 
-    const slug = node.fields.slug
-
-    if (slug.includes('home/')) {
-      template = homePageTemplate
-      path = slug.replace('home/', '')
+    if (node.slug === 'home') {
+      template = landingPageTemplate
+      path = '/'
     }
 
-    if (template) {
+    if (template && path) {
       createPage({
         path,
         component: template,
@@ -132,26 +64,59 @@ export const createPages: GatsbyNode['createPages'] = async ({
       })
     }
   })
-}
 
-export const onCreateNode: GatsbyNode['onCreateNode'] = ({
-  node,
-  getNode,
-  actions,
-}) => {
-  const { createNodeField } = actions
+  /* Contentful Rich Text Pages */
+  const contentfulRichTextPagesResult = await graphql<
+    Queries.ContentfulRichTextPagesQuery,
+    Queries.ContentfulRichTextPagesQueryVariables
+  >(
+    `
+      query ContentfulRichTextPages($languageCode: String) {
+        allContentfulRichTextPage(
+          filter: { node_locale: { eq: $languageCode } }
+        ) {
+          nodes {
+            id
+            slug
+          }
+        }
+      }
+    `,
+    {
+      languageCode,
+    },
+  )
 
-  if (['Mdx', 'Json'].includes(node.internal.type)) {
-    const value = createFilePath({
-      node,
-      getNode,
-    })
-    createNodeField({
-      node,
-      name: 'slug',
-      value,
-    })
+  if (contentfulRichTextPagesResult.errors) {
+    console.error(contentfulRichTextPagesResult.errors)
+    throw Error(contentfulRichTextPagesResult.errors)
   }
+
+  /* Contentful Rich Text Page Template */
+  const contentfulRichTextPageTemplate = path.resolve(
+    __dirname,
+    'src',
+    'templates',
+    'contentful-rich-text-page',
+    'index.tsx',
+  )
+
+  contentfulRichTextPagesResult.data?.allContentfulRichTextPage.nodes.forEach(
+    node => {
+      const template = contentfulRichTextPageTemplate
+      const path = node.slug
+
+      if (path) {
+        createPage({
+          path,
+          component: template,
+          context: {
+            id: node.id,
+          },
+        })
+      }
+    },
+  )
 }
 
 export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
