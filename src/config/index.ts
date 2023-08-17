ca710f9a6a4b140d0db5527f0202a825db13481e
Derry Sucari <derrysucari01@gmail.com>
2023-08-17 09:54:31 -0500

    refactor(config): update site configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,48 +1,18 @@
-/* Icons */
-import FacebookIcon from '@icons/facebook.svg'
-import GitHubIcon from '@icons/github.svg'
-import LinkedInIcon from '@icons/linkedin.svg'
-import TwitterIcon from '@icons/twitter.svg'
-
-/* Fonts */
-import MontserratSemiBold from '@fonts/montserrat/montserrat-semi-bold.ttf'
-import MontserratRegular from '@fonts/montserrat/montserrat-regular.ttf'
-
-/* eslint-disable-next-line import/no-anonymous-default-export */
-export default {
-  domain: process.env.GATSBY_DOMAIN,
-  name: process.env.GATSBY_APP_NAME,
-  language: process.env.GATSBY_LANGUAGE,
-  url: process.env.GATSBY_URL_ROOT,
-  logoSrc: `${process.env.GATSBY_URL_ROOT}/images/logo.png`,
-  ogImageSrc: `${process.env.GATSBY_URL_ROOT}/images/og-image.png`,
-  year: new Date().getFullYear(),
-  apiHost: process.env.GATSBY_API_HOST,
-  fonts: [MontserratSemiBold, MontserratRegular],
-  networks: [
+const siteConfig = {
+  deployedTranslations: [
     {
-      label: 'Facebook link',
-      alt: 'Facebook',
-      to: 'https://www.facebook.com/SucariTech',
-      icon: FacebookIcon,
+      code: 'en',
+      name: 'English',
     },
     {
-      label: 'GitHub link',
-      alt: 'GitHub',
-      to: 'https://github.com/SucariTech',
-      icon: GitHubIcon,
-    },
-    {
-      label: 'LinkedIn link',
-      alt: 'LinkedIn',
-      to: 'https://www.linkedin.com/company/sucari-tech',
-      icon: LinkedInIcon,
-    },
-    {
-      label: 'Twitter link',
-      alt: 'Twitter',
-      to: 'https://twitter.com/SucariTech',
-      icon: TwitterIcon,
+      code: 'es',
+      name: 'Español',
     },
   ],
+  domainName: process.env.GATSBY_DOMAIN_NAME ?? 'sucari.tech',
+  name: process.env.GATSBY_APP_NAME,
+  apiHost: process.env.GATSBY_API_HOST,
+  languageCode: process.env.GATSBY_LANGUAGE_CODE ?? 'en',
 }
+
+export default siteConfig
