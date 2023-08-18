51767b20d711a86b3b8ec81d77aa8e4ca056c567
Derry Sucari <derrysucari01@gmail.com>
2023-08-18 12:51:50 -0500

    feat(theme): add light theme

---
+++ src/theme/light/index.ts
@@ -0,0 +1,9 @@
+/* Palette */
+import palette from './palette'
+
+/* Shared */
+import { createTheme } from '@theme/shared/styles'
+
+const lightTheme = createTheme(palette)
+
+export default lightTheme
