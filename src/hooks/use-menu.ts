4cdb2df8e1b148f6de08e9f91f61b6efc1b7a9ab
Derry Sucari <110690597+derrysucari@users.noreply.github.com>
2023-08-18 15:55:54 -0500

    feat(hooks): add `use menu` hook (#22)

---
+++ src/hooks/use-menu.ts
@@ -0,0 +1,9 @@
+import React from 'react'
+
+/* Contexts */
+import { MenuContext } from '@contexts/menu-context'
+import type { MenuContextType } from '@contexts/menu-context'
+
+const useMenu = (): MenuContextType => React.useContext(MenuContext)
+
+export default useMenu
