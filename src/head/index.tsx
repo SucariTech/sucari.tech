97c23eab0c46e5b9c39ff86688daf2a322473dd2
Derry Sucari <derrysucari01@gmail.com>
2023-08-18 14:05:04 -0500

    feat(head): improve `head` component

--- src/head/index.tsx
+++ src/head/index.tsx
@@ -3,51 +3,123 @@ import React from 'react'
 /* Config */
 import config from '@config'
 
-export interface SiteHeadProps extends React.PropsWithChildren {
-  canonicalUrl: string
-  description: string
-  keywords: Array<string>
-  title: string
+/* Gatsby */
+import type { HeadProps } from 'gatsby'
+
+/* Normalize URL */
+import normalizeURL from 'normalize-url'
+
+/* Theme */
+import typography from '@theme/shared/typography'
+import type { ThemeFont } from '@theme/shared/typography'
+
+export interface SiteHeadProps
+  extends React.PropsWithChildren,
+    Pick<HeadProps, 'location'> {
+  canonicalURL?: string
+  description?: string | null
+  openGraphImageSource?: string | null
+  title?: string | null
+  twitterUsername?: string | null
 }
 
-const SiteHead: React.FC<SiteHeadProps> = ({
-  canonicalUrl,
-  children,
-  description,
-  keywords = [],
-  title,
-}) => {
+export const getDomainName = (languageCode: string): string =>
+  `${languageCode === 'en' ? '' : languageCode + '.'}${config.domainName}`
+
+const SiteHead: React.FC<SiteHeadProps> = props => {
+  const siteDomainName = getDomainName(config.languageCode)
+  const canonicalURL =
+    normalizeURL(
+      props.canonicalURL?.length
+        ? props.canonicalURL
+        : `https://${siteDomainName}${props.location.pathname}`,
+    ) + '/'
+  const shouldPreventIndexing =
+    !config.languageCode ||
+    (config.languageCode !== 'en' &&
+      !config.deployedTranslations.find(
+        ({ code }) => code === config.languageCode,
+      ))
+  const title = props.title?.length
+    ? `${props.title} | ${config.name}`
+    : config.name
   return (
     <React.Fragment>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta name="google" content="notranslate" />
-
       {/* Canonical URL */}
-      {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
-      {canonicalUrl && <meta property="og:url" content={canonicalUrl} />}
+      <link rel="canonical" href={canonicalURL} />
+      <meta property="og:url" content={canonicalURL} />
 
-      {
-        /* Keywords */
-        keywords.length && (
-          <meta name="keywords" content={keywords.join(', ')} />
-        )
-      }
+      <link
+        rel="alternate"
+        href={canonicalURL.replace(siteDomainName, getDomainName('en'))}
+        hrefLang="x-default"
+      />
 
-      {/* Open Graph */}
-      <meta name="og:description" content={description} />
-      <meta property="og:site_name" content={config.name} />
+      {config.deployedTranslations.map((language, index) => (
+        <link
+          key={index}
+          rel="alternate"
+          hrefLang={language.code}
+          href={canonicalURL.replace(
+            siteDomainName,
+            getDomainName(language.code),
+          )}
+        />
+      ))}
+
+      {/* Description */}
+      {!!props.description && (
+        <React.Fragment>
+          <meta name="description" content={props.description} />
+          <meta property="og:description" content={props.description} />
+          <meta name="twitter:description" content={props.description} />
+        </React.Fragment>
+      )}
+
+      {/* Open Graph Image */}
+      {!!props.openGraphImageSource && (
+        <React.Fragment>
+          <meta property="og:image" content={props.openGraphImageSource} />
+          <meta name="twitter:image" content={props.openGraphImageSource} />
+        </React.Fragment>
+      )}
+
+      {/* Should Prevent Indexing */}
+      {shouldPreventIndexing && <meta name="robots" content="noindex" />}
+
+      {/* Title */}
+      <title>{title}</title>
       <meta property="og:title" content={title} />
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content={config.ogImageSrc} />
+      <meta name="twitter:title" content={title} />
 
-      {
-        /* Fonts */
-        config.fonts.map((src, index) => (
-          <link key={index} href={src} rel="preload" as="font" crossOrigin="" />
-        ))
-      }
-      {children}
+      {/* Twitter Username */}
+      {!!props.twitterUsername && (
+        <React.Fragment>
+          <meta name="twitter:creator" content={props.twitterUsername} />
+          <meta name="twitter:site" content={props.twitterUsername} />
+        </React.Fragment>
+      )}
+
+      {/* Font Sources */}
+      {Object.entries<ThemeFont>(typography)
+        .map(([, value]) => value.sources)
+        .flat()
+        .map((source, index) => (
+          <link
+            key={index}
+            href={source.location}
+            rel="preload"
+            as="font"
+            crossOrigin=""
+          />
+        ))}
+
+      <meta name="google" content="notranslate" />
+      <meta property="og:site_name" content={config.name} />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+
+      {props.children}
     </React.Fragment>
   )
 }
