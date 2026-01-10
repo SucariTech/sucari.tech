77290165a622a1e4809be6ceeeb38f2e6482ca5d
Derry <derrysucari01@sucari.tech>
2026-01-09 22:42:42 -0500

    feat(config): add site configuration

---
+++ beta/src/config/index.ts
@@ -0,0 +1,19 @@
+const siteConfig = {
+  apiHost: process.env.NEXT_PUBLIC_API_HOST,
+  deployedTranslations: [
+    {
+      code: 'en',
+      name: 'English',
+    },
+    {
+      code: 'es',
+      name: 'Español',
+    },
+  ],
+  domainName: process.env.NEXT_PUBLIC_DOMAIN_NAME ?? 'sucari.tech',
+  googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+  languageCode: process.env.NEXT_PUBLIC_LANGUAGE_CODE ?? 'en',
+  name: process.env.NEXT_PUBLIC_APP_NAME ?? 'Sucari Tech',
+}
+
+export default siteConfig
