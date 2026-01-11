872d6d402b0b3dc81cfb85edc7558ce2af0996e2
Derry <derrysucari01@sucari.tech>
2026-01-11 16:31:05 -0500

    feat(components): add `logo` component

---
+++ beta/src/components/logo/index.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+/* MUI */
+import { createSvgIcon } from '@mui/material/utils'
+
+const Logo = createSvgIcon(
+  <svg
+    fill="currentColor"
+    viewBox="0 0 60.134 60.134"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="m36.905 15.21.749-.749-5.239-5.238-3.741 3.742 3.741 3.741.749-.748-2.994-2.993 2.245-2.245z"
+      transform="matrix(5.73967 0 0 5.73967 -155.986 -52.936)"
+    />
+    <path
+      d="m27.925 13.713-.748.748 5.238 5.239 3.742-3.742-3.742-3.742-.748.749 2.993 2.993-2.245 2.245z"
+      transform="matrix(5.73967 0 0 5.73967 -155.986 -52.936)"
+    />
+  </svg>,
+  'Logo',
+)
+
+export default Logo
