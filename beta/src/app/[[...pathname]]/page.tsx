57bf28daec33f1048d2f461d054ce89e79cab836
Derry Sucari <110690597+derrysucari@users.noreply.github.com>
2026-01-12 10:57:32 -0500

    feat(components/contentful-landing-page): add banner component (#65)

---
+++ beta/src/app/[[...pathname]]/page.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+/* Components */
+import BannerComponent from '@/components/contentful-landing-page/banner'
+
+/* Config */
+import config from '@/config'
+
+/* Generated Contentful */
+import type { ContentfulLandingPageQuery } from '@/generated/contentful'
+
+/* Next.js */
+import { notFound } from 'next/navigation'
+
+/* Utils */
+import contentfulClient from '@/utils/contentful/client'
+
+const contentfulComponentMap = new Map<string, React.ElementType>([
+  ['BannerComponent', BannerComponent],
+])
+
+const prepareContentfulComponents = (
+  data?: NonNullable<
+    ContentfulLandingPageQuery['pageCollection']
+  >['items'][number],
+): Array<React.ReactNode> | undefined => {
+  return data?.sectionsCollection?.items.map((section, index) => {
+    const typename = section?.content?.__typename ?? ''
+    const Component = contentfulComponentMap.get(typename)
+    if (Component) {
+      return <Component key={`${typename}-${index}`} {...section?.content} />
+    }
+    return null
+  })
+}
+
+const LandingPage: React.FC<PageProps<'/[[...pathname]]'>> = async ({
+  params,
+}) => {
+  const pathnameParam = (await params).pathname
+  const pathname = pathnameParam?.length
+    ? '/' + pathnameParam?.join('/') + '/'
+    : '/'
+
+  const result = await contentfulClient.ContentfulLandingPage({
+    languageCode: config.languageCode,
+    pathname,
+  })
+
+  const data = result.pageCollection?.items[0]
+
+  if (!data) {
+    notFound()
+  }
+
+  return prepareContentfulComponents(data)
+}
+
+export const generateStaticParams = async () => {
+  const result = await contentfulClient.ContentfulPages({
+    languageCode: config.languageCode,
+  })
+
+  const params: Array<{
+    pathname?: RegExpMatchArray
+  }> = []
+
+  result.pageCollection?.items.forEach(item => {
+    if (item?.pathname) {
+      params.push({
+        pathname: item.pathname.match(/[^/]+/g) ?? undefined,
+      })
+    }
+  })
+
+  return params
+}
+
+export default LandingPage
